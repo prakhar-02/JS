f3e59fe621f63700f0bf8a0e7523fb0a0fa58cf6
prakhar <sprakhar321@gmail.com>
2021-05-03 15:50:37 +0530

    Task7

---
+++ Task7/main.js
@@ -0,0 +1,15 @@
+//assign your age to variable "myAge"
+const myAge = 21;
+//assign 2 to variable "earlyYears"
+var earlyYears = 2;
+var earlyYears = earlyYears*10.5;
+//subtract 2 from myAge and assign it to variable "laterYears"
+var laterYears = myAge-2;
+//multiply 4 to "laterYears" and assign to itself
+var laterYears = laterYears*4;
+// assign sum of "laterYears" and "earlyYears" to "myAgeInDogYears"
+const myAgeInDogYears = laterYears+earlyYears;
+//convert name to lower case and assign to myName
+const myName = "Prakhar".toLowerCase();
+//display the name, age and age in dog years as a string
+console.log("My name is "+myName+". I am "+myAge+" years old in human years which is "+myAgeInDogYears+" years old in dog years.");
